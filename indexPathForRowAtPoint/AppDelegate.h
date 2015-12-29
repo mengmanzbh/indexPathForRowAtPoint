1460385dfe3dc37af195e8a7e4075771778af7cc
mengmanzbh <1934068305@qq.com>
2015-12-29 23:30:09 +0800

    Initial Commit

---
+++ indexPathForRowAtPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  indexPathForRowAtPoint
+//
+//  Created by 谢霆锋 on 15/12/29.
+//  Copyright © 2015年 lll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ indexPathForRowAtPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  indexPathForRowAtPoint
+//
+//  Created by 谢霆锋 on 15/12/29.
+//  Copyright © 2015年 lll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
