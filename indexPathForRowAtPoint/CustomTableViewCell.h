9e121594d85835a8de15b1597fc41a2c2e836b4e
mengmanzbh <1934068305@qq.com>
2015-12-29 23:48:58 +0800

    比代理更好的方法
    比代理更好的方法

---
+++ indexPathForRowAtPoint/CustomTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.h
+//  indexPathForRowAtPoint
+//
+//  Created by 谢霆锋 on 15/12/29.
+//  Copyright © 2015年 lll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIButton *btn;
+
+@end
--- indexPathForRowAtPoint/ViewController.m
+++ indexPathForRowAtPoint/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "CustomTableViewCell.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableA;
 
 @end
 
@@ -16,9 +18,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.tableA registerNib:[UINib nibWithNibName:@"CustomTableViewCell" bundle:nil] forCellReuseIdentifier:@"CustomTableViewCell"];
 }
-
+#pragma mark UITableViewDataSource and UITableViewDelegate Methods
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 1;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    return 10;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CustomTableViewCell";
+    CustomTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[CustomTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+  
+    [cell.btn addTarget:self action:@selector(changeLockState:) forControlEvents:UIControlEventTouchUpInside];
+    
+    return cell;
+}
+-(void)changeLockState:(id)sender{
+    
+    CGPoint switchOriginInTableView = [sender convertPoint:CGPointZero toView:self.tableA];
+    NSIndexPath *indexPath = [self.tableA indexPathForRowAtPoint:switchOriginInTableView];
+    NSLog(@"%ld",(long)indexPath.row);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
